1431b9070aa29ca413c4d9ea4eab77a283555df6
Ryo Murakami <integral14@gmail.com>
2018-05-11 19:31:21 +0900

    add compe template

---
+++ .vim/template/template_compe.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#define FOR(i, first, last) for (int i = (first); i < (last); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define CLR(a) memset((a), 0, sizeof(a))
+#define DUMP(x) std::cerr << #x << " = " << (x) << std::endl;
+#define DUMPR(v) REP(_i,v.size()){std::cerr<< #v << "[" << _i << "] = " << (v[_i]) << std::endl;}
+#define CIN_VEC(v,N) {v.clear(); v.reserve(N); REP(i,N){typename decltype(v)::value_type x; cin>>x; v.emplace_back(x);}};
+
+inline int to_int(string s) {
+  int v;
+  istringstream sin(s);
+  sin >> v;
+  return v;
+}
+template <class T> inline string to_string(T x) {
+  ostringstream sout;
+  sout << x;
+  return sout.str();
+}
+
+typedef unsigned long long ull;
+typedef unsigned long ul;
+
+int main() {
+  <+CURSOR +>
+
+      return 0;
+}
